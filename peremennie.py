e54cca1df039dc8cb8f39da622722ea1398d258d
VEfimov <xzxzxd83@gmail.com>
2024-05-20 18:47:05 +0300

    Add files via upload

---
+++ peremennie.py
@@ -0,0 +1,5 @@
+number_of_homework=12
+number_of_hours_spent=1.5
+course_name='Курс: Python'
+Time_task=number_of_hours_spent/number_of_homework
+print(course_name,',всего задач:',number_of_homework,'затрачено часов:',number_of_hours_spent,'среднее время выполнения',Time_task)
